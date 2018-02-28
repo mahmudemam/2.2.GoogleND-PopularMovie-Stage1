62b0f154dfc1caf491c02264e38b6e1822e91310
Mahmoud Emam <mahmoud.emam@ashtrywla.com>
2018-02-28 21:24:28 +0200

    adds handling of requesting TMDd for populer movies

--- app/src/main/java/com/udacity/examples/popularmovie/MainActivity.java
+++ app/src/main/java/com/udacity/examples/popularmovie/MainActivity.java
@@ -1,16 +1,18 @@
 package com.udacity.examples.popularmovie;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.udacity.examples.popularmovie.data.Movie;
-import com.udacity.examples.popularmovie.utils.MockMoviesUtils;
+import com.udacity.examples.popularmovie.utils.JsonUtils;
+import com.udacity.examples.popularmovie.utils.NetworkUtils;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MoviesAdapter.OnItemClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -24,11 +26,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.OnI
 
         moviesRecyclerView = findViewById(R.id.rv_movies);
 
-        MoviesAdapter adapter = new MoviesAdapter(this, this, Arrays.asList(MockMoviesUtils.MOCK_MOVIES));
-        moviesRecyclerView.setAdapter(adapter);
-
-        moviesRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        moviesRecyclerView.setHasFixedSize(true);
+        new FetchingMovieTask().execute(new Void[]{null});
     }
 
     @Override
@@ -44,5 +42,23 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.OnI
         detailsIntent.putExtra(DetailsActivity.INTENT_MOVIE_SYNOPSIS_KEY, movie.getOverview());
 
         startActivity(detailsIntent);
+    }
+
+    public class FetchingMovieTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            return NetworkUtils.loadPopularMovies();
+        }
+
+        @Override
+        protected void onPostExecute(String jsonStr) {
+            List<Movie> movies = JsonUtils.parseMovies(jsonStr);
+
+            MoviesAdapter adapter = new MoviesAdapter(MainActivity.this, MainActivity.this, movies);
+            moviesRecyclerView.setAdapter(adapter);
+
+            moviesRecyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 3));
+            moviesRecyclerView.setHasFixedSize(true);
+        }
     }
 }
---
+++ app/src/main/java/com/udacity/examples/popularmovie/utils/JsonUtils.java
@@ -0,0 +1,70 @@
+package com.udacity.examples.popularmovie.utils;
+
+import android.util.Log;
+
+import com.udacity.examples.popularmovie.data.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by noname on 2/28/18.
+ */
+
+public class JsonUtils {
+    private static final String TAG = JsonUtils.class.getSimpleName();
+
+    public static List<Movie> parseMovies(String jsonStr) {
+        List<Movie> movies = null;
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonStr);
+
+            JSONArray results = jsonObject.optJSONArray("results");
+
+            if (results != null) {
+                movies = new ArrayList<>(results.length());
+
+                for (int i = 0; i < results.length(); i++) {
+                    JSONObject movieObject = results.getJSONObject(i);
+
+                    JSONArray genresArray = movieObject.optJSONArray("genre_ids");
+                    int[] genres = null;
+
+                    if (genresArray != null) {
+                        genres = new int[genresArray.length()];
+
+                        for (int j = 0; j < genresArray.length(); j++) {
+                            genres[j] = genresArray.optInt(j);
+                        }
+                    }
+
+                    movies.add(new Movie(
+                            movieObject.optInt("vote_count", 0),
+                            movieObject.optInt("id", 0),
+                            movieObject.optBoolean("video", false),
+                            movieObject.optDouble("vote_average", 0d),
+                            movieObject.optString("title", ""),
+                            movieObject.optDouble("popularity", 0d),
+                            movieObject.optString("poster_path", ""),
+                            movieObject.optString("original_language", ""),
+                            movieObject.optString("original_title", ""),
+                            genres,
+                            movieObject.optString("backdrop_path", ""),
+                            movieObject.optBoolean("adult", false),
+                            movieObject.optString("overview", ""),
+                            movieObject.optString("release_date", "")));
+
+                }
+            }
+        } catch (JSONException ex) {
+            Log.e(TAG, "parseMovies: ", ex);
+        }
+
+        return movies;
+    }
+}
