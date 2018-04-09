ccf6850f4de6bd6e58f4ceaa6567da34ba7fbdec
Mahmoud Emam <mahmoud.emam@ashtrywla.com>
2018-04-09 19:50:57 +0200

    refactor video activity by removing tabs

--- app/src/main/java/com/udacity/examples/popularmovie/VideosActivity.java
+++ app/src/main/java/com/udacity/examples/popularmovie/VideosActivity.java
@@ -3,18 +3,9 @@ package com.udacity.examples.popularmovie;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.udacity.examples.popularmovie.data.Movie;
 import com.udacity.examples.popularmovie.utils.MockMoviesUtils;
@@ -22,22 +13,6 @@ import com.udacity.examples.popularmovie.utils.MockMoviesUtils;
 import java.util.Arrays;
 
 public class VideosActivity extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
-
     public static final String MOVIE_ID_KEY = "movie_id";
 
     @Override
@@ -54,95 +29,22 @@ public class VideosActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_videos);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        RecyclerView videoRecyclerView = findViewById(R.id.rv_videos);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        videoRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        VideosAdapter adapter = new VideosAdapter(this, Arrays.asList(MockMoviesUtils.MOCK_VIDEOS),
 
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(final LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_videos, container, false);
-
-            RecyclerView videoRecyclerView = rootView.findViewById(R.id.rv_videos);
-
-            videoRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-
-            VideosAdapter adapter = new VideosAdapter(getContext(), Arrays.asList(MockMoviesUtils.MOCK_VIDEOS),
-
-                    new VideosAdapter.OnVideoClickLinstener() {
-                        @Override
-                        public void onVideoClick(Movie.Video video) {
-                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=nJCc5HRPxYA"));
-                            if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-                                startActivity(intent);
-                            }
+                new VideosAdapter.OnVideoClickLinstener() {
+                    @Override
+                    public void onVideoClick(Movie.Video video) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=nJCc5HRPxYA"));
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            startActivity(intent);
                         }
-                    });
-            videoRecyclerView.setAdapter(adapter);
+                    }
+                });
+        videoRecyclerView.setAdapter(adapter);
 
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
     }
 }
